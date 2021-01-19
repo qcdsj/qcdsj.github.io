74e0e8c1f922b0140cbeaa46b60a4bbc3972a58b
Dorong <lyseong99@sju.ac.kr>
2021-01-19 14:09:56 +0900

    posting

--- _site/assets/javascripts/lunr/lunr.store.js
+++ _site/assets/javascripts/lunr/lunr.store.js
@@ -1,1 +1,6 @@
-var store = []
+var store = [{
+        "title": "First post",
+        "excerpt":"이것은 내 첫번째 포스트 하 ","categories": ["Projects"],
+        "tags": ["project"],
+        "url": "https://qcdsj.github.io//projects/2021/01/19/sample_first.html"
+      }]
