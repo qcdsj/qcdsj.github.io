78cc179e1c76a0e55a7577ce0c37e250d3155f89
Dorong <lyseong99@sju.ac.kr>
2021-01-19 11:26:29 +0900

    commit

---
+++ assets/javascripts/lunr/lunr.store.js
@@ -0,0 +1,11 @@
+var store = [{
+        "title": "example post",
+        "excerpt":"You’ll find this post in your _posts directory. Go ahead and edit it and re-build the site to see your changes. You can rebuild the site in many different ways, but the most common way is to run jekyll serve, which launches a web server and auto-regenerates your site when...","categories": ["jekyll","update"],
+        "tags": [],
+        "url": "https://qcdsj.github.io/jekyll/update/2021/01/18/example.html"
+      },{
+        "title": "Welcome to Jekyll!",
+        "excerpt":"You’ll find this post in your _posts directory. Go ahead and edit it and re-build the site to see your changes. You can rebuild the site in many different ways, but the most common way is to run jekyll serve, which launches a web server and auto-regenerates your site when...","categories": ["jekyll","update"],
+        "tags": [],
+        "url": "https://qcdsj.github.io/jekyll/update/2021/01/18/welcome-to-jekyll.html"
+      }]
