4f37438e34c8e5d204f15119d34f7db9b1b409bc
Dorong <lyseong99@sju.ac.kr>
2021-01-19 14:33:19 +0900

    second try

--- _site/assets/javascripts/lunr/lunr.store.js
+++ _site/assets/javascripts/lunr/lunr.store.js
@@ -1,6 +1,11 @@
 var store = [{
         "title": "First post",
-        "excerpt":"이것은 내 첫번째 포스트 하 ","categories": ["Projects"],
+        "excerpt":"This is a Sample post. ","categories": ["Projects"],
         "tags": ["project"],
         "url": "https://qcdsj.github.io//projects/2021/01/19/sample_first.html"
+      },{
+        "title": "Second post",
+        "excerpt":"Second version of Sample post. ","categories": ["Projects"],
+        "tags": ["project"],
+        "url": "https://qcdsj.github.io//projects/2021/01/19/sample_second.html"
       }]
